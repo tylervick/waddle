3c2f55250f66f3940d82bb9295020d419aba5007
Tyler Vick <tyler@tylervick.com>
2026-07-28 10:15:02 -0700

    feat(wad): lump directory index with byte offsets and file-handle reads

---
+++ App/Tests/WADLumpIndexTests.swift
@@ -0,0 +1,50 @@
+import XCTest
+@testable import WADdle
+
+final class WADLumpIndexTests: XCTestCase {
+    // Builds a minimal WAD: header + one lump "DATA" (bytes) + directory.
+    private func makeWAD(lumpName: String, lump: [UInt8]) -> Data {
+        var d = Data()
+        func i32(_ v: Int) -> Data { withUnsafeBytes(of: Int32(v).littleEndian) { Data($0) } }
+        let headerSize = 12
+        let lumpOffset = headerSize
+        let dirOffset = headerSize + lump.count
+        d.append(contentsOf: Array("IWAD".utf8))       // magic
+        d.append(i32(1))                                // numLumps
+        d.append(i32(dirOffset))                        // dirOffset
+        d.append(contentsOf: lump)                      // lump bytes at offset 12
+        d.append(i32(lumpOffset))                       // dir entry: filepos
+        d.append(i32(lump.count))                       // dir entry: size
+        var name8 = Array(lumpName.utf8); name8 += Array(repeating: 0, count: 8 - name8.count)
+        d.append(contentsOf: name8)                     // dir entry: name (8, NUL-padded)
+        return d
+    }
+
+    func testFindsLumpOffsetAndSize() throws {
+        let wad = makeWAD(lumpName: "DATA", lump: [0xAA, 0xBB, 0xCC])
+        let index = try WADLumpIndex(wad: wad)
+        let os = index.offsetSize(of: "DATA")
+        XCTAssertEqual(os?.offset, 12)
+        XCTAssertEqual(os?.size, 3)
+        XCTAssertNil(index.offsetSize(of: "NOPE"))
+    }
+
+    func testLumpNameIsUppercasedAndLookupCaseInsensitive() throws {
+        let wad = makeWAD(lumpName: "TITLEPIC", lump: [1, 2])
+        let index = try WADLumpIndex(wad: wad)
+        XCTAssertNotNil(index.offsetSize(of: "titlepic"))
+    }
+
+    func testReadsLumpBytesFromFileHandle() throws {
+        let wad = makeWAD(lumpName: "DATA", lump: [0xAA, 0xBB, 0xCC])
+        let url = FileManager.default.temporaryDirectory.appendingPathComponent("\(UUID()).wad")
+        try wad.write(to: url); defer { try? FileManager.default.removeItem(at: url) }
+        let index = try WADLumpIndex.read(from: url)
+        XCTAssertEqual(try WADLumpIndex.lumpData("DATA", at: url, index: index).map(Array.init), [0xAA, 0xBB, 0xCC])
+    }
+
+    func testRejectsBadMagic() {
+        var d = Data("XXXX".utf8); d.append(Data(repeating: 0, count: 8))
+        XCTAssertThrowsError(try WADLumpIndex(wad: d))
+    }
+}
